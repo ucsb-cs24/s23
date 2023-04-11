eba460162f54dc3acfba38e1c94c09d058d5ec56
Kevin Burk <xavierholt@gmail.com>
2023-04-11 15:43:39 -0700

    FibVec Lab

---
+++ fibvec/FibVec.cpp
@@ -0,0 +1,14 @@
+#include "FibVec.h"
+
+// This provides exception types:
+#include <stdexcept>
+
+
+FibVec* create_fibvec() {
+  // Used by the autograder to make a new FibVec.
+  // You shouldn't need to change this.
+  return new FibVec();
+}
+
+
+// FibVec Function Implementations
---
+++ fibvec/FibVec.h
@@ -0,0 +1,22 @@
+#ifndef FIBVEC_H
+#define FIBVEC_H
+
+// This provides the size_t type:
+#include <cstddef>
+
+class FibVec {
+  // Member Variables
+
+  // Helper Functions
+
+public:
+  // Constructor and Destructor
+
+  // Member Functions
+
+};
+
+// Used by the autograder:
+FibVec* create_fibvec();
+
+#endif
